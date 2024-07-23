d8002c86a85b8a122509c9ac999161e9ad1fa0df
Eugene <eugenemutuyimana15@gmail.com>
2024-07-23 21:43:22 +0200

    added models getters

--- core_app/models.py
+++ core_app/models.py
@@ -1,13 +1,43 @@
+from datetime import timedelta
+import re
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    email=models.EmailField()
+    first_name=models.CharField(max_length=256, null=False)
+    last_name=models.CharField(max_length=256, null=False)
     phone_number = models.CharField(max_length=10, null=False, blank=False)
+    is_editor=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+    
+    @property
+    def is_admin(self):
+        """Return True if user is admin"""
+        return self.is_superuser
+    
+    @property
+    def is_editor(self):
+        """Return True if user is editor"""
+        return self.is_editor
 
 class Subscription(models.Model):
     identifier=models.IntegerField()
-    duration=models.DateTimeField(auto_now_add=True)
     verbose=models.CharField(max_length=10)
+    date_created=models.DateTimeField(auto_now_add=True)
+    duration=models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.verbose
+    
+    @property
+    def is_subscription_valid(self):
+        """Check if user is still able to work on tests"""
+        return self.date_created < ( self.date_created + timedelta(hours=self.duration))
+    
 
 class Guest(models.Model):
     phone_number=models.CharField(max_length=10, null=False, blank=False)
@@ -17,10 +47,39 @@ class Guest(models.Model):
     last_active=models.DateTimeField(null=True)
     date_created=models.DateTimeField(auto_now_add=True)
 
-    def is_code_used(self):
-        """Check if code is used atleast once"""
-        pass
+    def __str__(self):
+        return self.phone_number
+    
+    @property
+    def is_subscription_active(self):
+        """Check if guest user subscription is active"""
+        return self.subscription.is_subscription_valid()
 
+    @property
+    def is_code_expired(self):
+        """Check if code is used atleast once"""
+        try:
+            sub = self.subscription.is_subscription_valid()
+        except Exception:
+            return False
+        if not sub:
+            self.code=None
+            self.code_used_count=0
+            self.subscription=None
+            self.save()
+
+            return True
+        return False
+
+    @property
+    def is_code_valid(self):
+        """Check if code entered by user is valid one"""
+        code_length = len(self.code)
+        pattern = r'AVI'
+        found = re.findall(pattern=pattern, string=self.code)
+
+        return (pattern in found) & (code_length == 10)
+    
 class Question(models.Model):
     question=models.CharField(max_length=256)
     correct_answer=models.CharField(max_length=256)
@@ -29,6 +88,16 @@ class Question(models.Model):
     dummy_answer_3=models.CharField(max_length=256)
     date_created=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.question
+    
+    @property
+    def get_random(self):
+        """Return random questions"""
+        queryset=self.objects.order_by('?')[:20]
+        return queryset
+    
+    
 class GuestPerformance(models.Model):
     question=models.ForeignKey(Question, on_delete=models.CASCADE)
     got_it_right=models.PositiveIntegerField()
