5a233d132e7cdc3c567e6bd597056513f298f964
Eugene <eugenemutuyimana15@gmail.com>
2024-07-23 21:43:01 +0200

    help module

---
+++ core_app/utils.py
@@ -0,0 +1,10 @@
+import random
+import json
+
+def shuffle_output(queryset):
+    """Return shuffled question answers"""
+    pass
+
+def dump_dict(dict):
+    """Return json format of passed dictionary"""
+    pass
