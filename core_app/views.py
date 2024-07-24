a401c66ff62953170bb0622842aeffd46b8ada03
Eugene <eugenemutuyimana15@gmail.com>
2024-07-24 17:43:48 +0200

    initial views set up

--- core_app/views.py
+++ core_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request=request, template_name='home.html')
+
+
+def exam(request):
+    return render(request=request, template_name='exam.html')
+
+def contact_us(request):
+    return render(request=request, template_name='contact-us.html')
+
+def blog(request):
+    return render(request=request, template_name='blog.html')
+
+
+def payment(request):
+    return render(request=request, template_name='payment.html')
