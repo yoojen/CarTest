ce97df0cfb248a7b6162107ec726be41595473ee
Eugene <eugenemutuyimana15@gmail.com>
2024-07-24 12:13:40 +0200

    before working on views

--- core_app/utils.py
+++ core_app/utils.py
@@ -1,10 +1,25 @@
-import random
 import json
+from ast import literal_eval
+import random
 
-def shuffle_output(queryset):
-    """Return shuffled question answers"""
-    pass
+def generate_random_code()->str:
+    """Return combined hex and immutable pattern random codes"""
+    immutable_pattert = 'AVI' # To be stored in env
+    random = random.randint(0, 9_999_999)
+    return f"{immutable_pattert}{random}"
 
-def dump_dict(dict):
+def transform_code_to_hex(code: str)->str:
+    """Transform code to hex values"""
+    code_hex_version = hex(code)
+    return code_hex_version
+
+def unpack_code(code: str)->str:
+    """Return decoded str to match guest code"""
+    immutable_pattert = 'AVI'  # To be stored in env
+    code_portion = code[3:]
+    code_to_integer = literal_eval(code_portion)
+    return f"{immutable_pattert}{code_to_integer}"
+
+def dump_dict(dict: dict):
     """Return json format of passed dictionary"""
     pass
