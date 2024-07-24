1ac6ef22a74b632faac6285a0cd326700cf42962
Eugene <eugenemutuyimana15@gmail.com>
2024-07-24 12:13:26 +0200

    before working on views

--- core_app/models.py
+++ core_app/models.py
@@ -1,8 +1,20 @@
-from datetime import timedelta
+# Utilities
+from .utils import generate_random_code
+
+# Python code modules
+from datetime import datetime, timedelta
 import re
 
+# Django modules
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
+
+PAYMENT_METHOD_CHOICES = (
+    ("MTN", "MTN MoMo"),
+    ("AIRTEL", "Airtel Money")
+)
 
 class User(AbstractUser):
     email=models.EmailField()
@@ -79,13 +91,21 @@ class Guest(models.Model):
         found = re.findall(pattern=pattern, string=self.code)
 
         return (pattern in found) & (code_length == 10)
+
+    def generate_code(self):
+        """Generate operational code"""
+        code = generate_random_code()
+        self.code = code
+        self.save()
     
+    def re_generate_code(self):
+        """Regenerates new code"""
+        self.generate_code()
+
+
 class Question(models.Model):
     question=models.CharField(max_length=256)
     correct_answer=models.CharField(max_length=256)
-    dummy_answer_1=models.CharField(max_length=256)
-    dummy_answer_2=models.CharField(max_length=256)
-    dummy_answer_3=models.CharField(max_length=256)
     date_created=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -97,6 +117,19 @@ class Question(models.Model):
         queryset=self.objects.order_by('?')[:20]
         return queryset
     
+class Answers(models.Model):
+    question=models.ForeignKey(Question, on_delete=models.CASCADE)
+    dummy_answer_1 = models.CharField(max_length=256)
+    dummy_answer_2 = models.CharField(max_length=256)
+    dummy_answer_3 = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.question
+    
+    def shuffle_dummy_answers(self):
+        """Return random questions"""
+        queryset = self.objects.order_by('?')
+        return queryset
     
 class GuestPerformance(models.Model):
     question=models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -104,11 +137,50 @@ class GuestPerformance(models.Model):
     got_it_wrong=models.PositiveIntegerField()
     date_created=models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Got it right: {self.got_it_right} vs got it wrong: {self.got_it_wrong}"
+
+    @property
+    def got_it_right_proportion(self):
+        """Return 20% proportion"""
+        total_response = self.got_it_right + self.got_it_wrong
+        proportion = (total_response * 100) / self.got_it_right
+        return proportion
+    
+    @property
+    def got_it_wrong_proportion(self):
+        """Return 20% proportion"""
+        total_response = self.got_it_right + self.got_it_wrong
+        proportion = (total_response * 100) / self.got_it_wrong
+        return proportion
+    
+    @property
+    def get_total(self):
+        """Return total number of response"""
+        return self.got_it_right + self.got_it_wrong
+# JSON IN PROGRESS QUESTIONS FORMAT
+# question={
+#     "question_id":{
+#         "answered": bool,
+#         "answer": "text",
+#     },
+#     "question_id":{
+#         "answered": bool,
+#         "answer": "text",
+#     }
+# }
 class InProgress(models.Model):
     guest=models.OneToOneField(Guest, on_delete=models.CASCADE)
     questions=models.JSONField()
     is_done=models.BooleanField(default=False)
     date_created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.guest
+    
+    @property
+    def get_elapsed_time(self):
+        return datetime.now - self.date_created
 
 class History(models.Model):
     guest = models.ForeignKey(Guest, null=True, on_delete=models.SET_NULL)
@@ -116,10 +188,6 @@ class History(models.Model):
     test_performance=models.JSONField()
     date_created=models.DateTimeField(auto_now_add=True)
 
-PAYMENT_METHOD_CHOICES=(
-    ("MTN", "MTN MoMo"), 
-    ("AIRTEL", "Airtel Money")
-)
 class Payments(models.Model):
     guest=models.ForeignKey(Guest, null=True, on_delete=models.SET_NULL)
     method = models.CharField(max_length=256, choices=PAYMENT_METHOD_CHOICES)
@@ -131,6 +199,7 @@ class Blog(models.Model):
     title=models.CharField(max_length=1024)
     content=models.CharField(max_length=2024)
     date_created=models.DateTimeField(auto_now_add=True)
+    slug=models.SlugField()
 
 class Comment(models.Model):
     blog=models.ForeignKey(Blog, on_delete=models.CASCADE)
