6fab9c3830f6dc0ced31c593c0080c6a829243ee
Eugene <eugenemutuyimana15@gmail.com>
2024-07-25 23:38:42 +0200

    worked on code gen

--- core_app/views.py
+++ core_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from .forms import GenerateCodeForm
+
 
 def home(request):
-    return render(request=request, template_name='home.html')
+    form = GenerateCodeForm()
+    return render(request=request, template_name='home.html', context={'form': form})
 
 
 def exam(request):
@@ -16,3 +20,20 @@ def blog(request):
 
 def payment(request):
     return render(request=request, template_name='payment.html')
+
+def generate_code(request):
+    if request.method == 'POST':
+        form = GenerateCodeForm(request.POST)
+        if form.is_valid():
+            import time
+            time.sleep(5)
+            print(form.cleaned_data)
+            # After receiving phone number, initialize payment, code generation and code is sent
+            # Redirect user to verify code page
+            return JsonResponse({"status": "ok"})
+        print(form.errors)
+        return redirect("core_app:home")
+
+
+def verify_code(request):
+    return render(request=request, template_name="verify_code.html")
