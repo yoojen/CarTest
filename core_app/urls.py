b9e687fe6ef4ce596838d9c7cc770bfe28ed3689
Eugene <eugenemutuyimana15@gmail.com>
2024-07-25 23:39:03 +0200

    urls done

--- core_app/urls.py
+++ core_app/urls.py
@@ -1,6 +1,6 @@
 from .views import (
         home, exam, contact_us,
-        blog, payment
+    blog, payment, generate_code, verify_code
     )
 from django.urls import path
 
@@ -11,5 +11,7 @@ urlpatterns=[
     path('contact_us/', contact_us, name='contact_us'),
     path('blog/', blog, name='blog'),
     path('payment/', payment, name='payment'),
+    path('gen-code/', generate_code, name="generate_code"),
+    path('continue/', verify_code, name="verify_code"),
 
 ]
