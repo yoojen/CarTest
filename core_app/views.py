43af4a93e2852f20f7a2704bd2226160196f4712
Eugene <eugenemutuyimana15@gmail.com>
2024-07-26 10:32:42 +0200

    code generation completed

--- core_app/views.py
+++ core_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from .forms import GenerateCodeForm
+from .models import Guest
 
 
 def home(request):
@@ -25,15 +26,20 @@ def generate_code(request):
     if request.method == 'POST':
         form = GenerateCodeForm(request.POST)
         if form.is_valid():
-            import time
-            time.sleep(5)
-            print(form.cleaned_data)
+            phone_number = form.cleaned_data['phone_number']
+            user, created = Guest.objects.get_or_create(phone_number=phone_number)
+            if not user:
+                form.add_error("User not created")
+            user.generate_code()
             # After receiving phone number, initialize payment, code generation and code is sent
             # Redirect user to verify code page
+            print(f"Sending code-> {user.code}")
+            import time
+            time.sleep(3)
             return JsonResponse({"status": "ok"})
-        print(form.errors)
         return redirect("core_app:home")
 
 
 def verify_code(request):
+    
     return render(request=request, template_name="verify_code.html")
